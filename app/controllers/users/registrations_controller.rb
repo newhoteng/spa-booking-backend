63fd2a517f2d4b78758e4fc3095f204595ab75b2
Harriet Oteng <harriet.oteng@gmail.com>
2023-08-18 18:08:23 +0000

    Add rack_sessions_fix.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,23 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   # clear_respond_to 
+  include RackSessionsFix
   respond_to :json
+
+  private
+
+  def respond_with(current_user, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: {code: 200, message: 'Signed up successfully.'},
+        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+      }
+    else
+      render json: {
+        status: {message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}"}
+      }, status: :unprocessable_entity
+    end
+  end
 
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
