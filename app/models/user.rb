403aedeb0cc47b61213984a25dcab544b7ca223c
Harriet Oteng <harriet.oteng@gmail.com>
2023-08-18 15:53:46 +0000

    Delete serializer file

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reservations, foreign_key: 'user_id', dependent: :destroy
-  # added throug: :reservations
+  # added through: :reservations
   has_many :spa_services, foreign_key: 'user_id', through: :reservations, dependent: :destroy
 
   validates_presence_of :username, :email
--- app/serializers/user_serializer.rb
+++
@@ -1,4 +0,0 @@
-class UserSerializer
-  include JSONAPI::Serializer
-  attributes :id, :email, :username, :created_at
-end
