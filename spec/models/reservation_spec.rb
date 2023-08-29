62e6ea595ed43df02030090415dfd581f5f0babc
fatiha <fathiato@gmail.com>
2023-08-29 12:14:01 +0100

    adding reservation spec

---
+++ spec/models/reservation_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Reservation, type: :model do
+  let(:user) { User.new(username: 'John Doe', email:'test@blog.com', password: 'password') }
+  let(:spa_service) do
+    SpaService.create(name: 'facials', image: 'www.google.com', description: 'helps to get rid of dead skin', price:100, duration: 60, is_removed: false)
+  end
+  let(:reserve) do
+    Reservation.create(user: user, spa_service: , date: Date.today(), city: 'Accra')
+  end
+
+  before { user.save }
+
+  it 'reservation should be valid' do
+    expect(reserve).to be_valid
+  end
+
+  it 'reservation should be valid' do
+    reserve.city = nil
+    expect(reserve).to_not be_valid
+  end
+
+  it 'reservation should be valid' do
+    reserve.date = nil
+    expect(reserve).to_not be_valid
+  end
+
+  it 'reservation should be valid' do
+    expect(reserve.date).to be_a(Date)
+  end
+
+
+end
