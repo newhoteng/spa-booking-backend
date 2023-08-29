8ee25552c966f378563b9664d606f094f5857c5a
fatiha <fathiato@gmail.com>
2023-08-29 11:18:09 +0100

    adding user specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { User.new(username: 'John Doe', email:'test@blog.com', password: 'password') }
+  before { user.save }
+
+  it 'user should be valid' do
+    expect(user).to be_valid
+  end
+
+  it 'name should be present' do
+    user.username = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'email is nil so user should not be valid' do
+    user.email = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'bio should be present' do
+    expect(user).to be_valid
+  end
+
+  it 'photo should be present' do
+    expect(user).to be_valid
+  end
+end
